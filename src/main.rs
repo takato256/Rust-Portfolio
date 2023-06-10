1bfb5ffb18cc98dd99821738a940088c827a2482
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-06-11 04:54:59 +0900

    インポート部分記入

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-}
+// yewフレームワークをインポート
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+// モジュールを定義
+mod components;
+mod content;
+mod generator;
+mod pages;
+
+// 他のモジュールから特定のコンポーネントや関数をインポート
+use pages::author::Author;
+use pages::author_list::AuthorList;
+use pages::home::Home;
+use pages::page_not_found::PageNotFound;
+use pages::post::Post;
+use pages::post_list::PostList;
+use yew::html::Scope;
+
+// 特定のトレイトの標準的な実装を提供
+// Routableのみyew-routerライブラリのトレイト
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+
+
