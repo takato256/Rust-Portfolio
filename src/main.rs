fae7d4ae7c9e53253aa9869af71d905c202061a0
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-06-22 16:02:30 +0900

    complete main.rs

--- src/main.rs
+++ src/main.rs
@@ -51,5 +51,114 @@ impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    
+    fn create(_ctx: &context<Self>) -> Self {
+        Self {
+            navbar_active: false,   // navbar_activeの初期状態がfalse
+        }
+    }
+
+    fn update(&mut self, _ctf: &Context<Self>, msg: Self::Message) -> bool{
+        match msg {
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<self>) -> Html {
+        html! {
+            <BrowserRouter>
+                { self.view_nav(ctx.link()) }
+
+                <main>
+                    <Switch<Router> render={switch} />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        { "Powered by "}
+                        <a href="https://yew.rs">{ "Yew" }</a>
+                        { "using" }
+                        <a href="https://bulma.io">{ "Bulma" }</a>
+                        { "and images from" }
+                        <a href="https://unsplash.com">{ "Unsplash" }</a>
+                    </div>
+                </footer>
+            </BrowserRouter>
+        }
+    }
+}
+
+impl App {
+    fn view_nav(&self, link: &Scope<Self>) -> Html {
+        let Self { navbar_active, .. } = *self;
+
+        let active_class = if !navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
+
+                    <button class={classes!("navbar-burger", "burger", active_class)}
+                        aria-label="menu" aria-expanded="false"
+                        onclick={link.callback(|_| Msg::ToggleNavbar)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class={classes!("navbar-menu", active_class)}>
+                    <div class="navbar-start">
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Home" }
+                        </Link<Route>>
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Posts}>
+                            { "Posts" }
+                        </Link<Route>>
+                        
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <div class="navbar-link">
+                                { "More" }
+                            </div>
+                            <div class="navbar-dropdown">
+                                <Link<Route> classes-{classes!("navbar-item")} to={Route::Authors}>
+                                    { "Meet the authors" }
+                                </Link<Route>>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Post { id } => {
+            html! { <Post seed={id} /> }
+        }
+        Route::Posts => {
+            html! { <PostList /> }
+        }
+        Route::Author { id } => {
+            html! { <Author seed={id} /> }
+        }
+        Route::Authors => {
+            html! { <AuthorList /> }
+        }
+        Route::Home => {
+            html! { <Home /> }
+        }
+        Route::NotFound => {
+            html! { <PageNotFound /> }
+        }
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    yew::Renderer::<App>::new().render();
 }
